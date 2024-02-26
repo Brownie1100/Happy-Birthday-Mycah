a837c27a3dd56f634ebb507632254aaa78bf6a19
Brownie1100 <90987118+Brownie1100@users.noreply.github.com>
2024-02-26 23:11:46 +0530

    mhm

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function Clickbutton() {
     event.preventDefault();
-    alert('Text Clicked!'); // Replace with your desired functionality
+ //   alert('Text Clicked!'); // Replace with your desired functionality
+    document.getElementById("message").style.display="block";
+}
+function close(){
+    document.getElementById("message").style.display="none";
 }
