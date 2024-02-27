719ffc9316c1f0f3a1fc775a367f55fd91255787
Brownie1100 <90987118+Brownie1100@users.noreply.github.com>
2024-02-28 00:13:31 +0530

    hmmm

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+var element = document.getElementById('myElement');
+element.style.display='none';
+
 function Clickbutton() {
     event.preventDefault();
  //   alert('Text Clicked!'); // Replace with your desired functionality
-    document.getElementById("message").style.display="block";
+  //  document.getElementById("message").style.display="block";
+    var element = document.getElementById('myElement');
+    if(element.style.display==="none"||element.style.display===""){
+        element.style.display='block';
+    }
+    else{
+        element.style.display='none';
+    }
 }
 function close(){
     document.getElementById("message").style.display="none";
