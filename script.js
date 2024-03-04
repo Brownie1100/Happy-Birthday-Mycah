a44ecda1435744d5aad03ff97fec42626546dad6
Brownie1100 <90987118+Brownie1100@users.noreply.github.com>
2024-03-04 22:30:36 +0530

    done

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ elementc.style.display='none';
 
 function Clickbutton() {
     event.preventDefault();
-    var element = document.getElementById('content-section');
-    var element2=document.getElementById("welcome-section-message");
-    if(element.style.display==="none"||element.style.display===""){
-        element.style.display='block';
-        element2.style.display="none";
-    }
-    else{
-        element.style.display='none';
-        element2.style.display="block";
+    var element=document.getElementById("welcome-section");
+    var element2 = document.getElementById('content-section');
+    if(element2.style.display==="none"||element2.style.display===""){
+        element2.style.display='block';
+        element.style.display="none";
     }
 }
 
 function Clickfooter(){
 
+}
+
+function ClickContentbutton(){
+    event.preventDefault();
+    var element=document.getElementById("welcome-section");
+    var element2 = document.getElementById('content-section');
+    if(element.style.display==="none"||element.style.display===""){
+        element.style.display='block';
+        element2.style.display="none";
+    }
 }
